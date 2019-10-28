e1d1a2c4f6e9e150ab63c701e3e26c916e6c3305
ParshakovaIrina <44544412+ParshakovaIrina@users.noreply.github.com>
2019-10-28 12:59:44 +0300

    lastFixed

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -12,7 +12,7 @@ export class BooksComponent implements OnInit {
   //books= BOOKS;
   selectedBook: Book;
   book = JSON.parse(localStorage.getItem("myKey"));
-   returnObj = JSON.parse(localStorage.getItem("myKey"));
+  
 
   constructor(private bookService: BookService) { }
   
@@ -28,11 +28,3 @@ export class BooksComponent implements OnInit {
     this.selectedBook = book;
   }
 }
-var returnObj = JSON.parse(localStorage.getItem("myKey"));
-  var returnObj1 = JSON.parse(localStorage.getItem("myKey1"));
-  console.log(localStorage.length);
-  let y=localStorage.length;
-  for(let i=0; i<y; i++){
-    const newLocal = returnObj.id;
-    console.log(newLocal);
-  }
